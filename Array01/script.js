e9d546135aa855f6f7b0a45348ce51182169557d
Guto-potato <gustavo.borghi18@gmail.com>
2024-10-17 11:26:27 -0300

    Array01

---
+++ Array01/script.js
@@ -0,0 +1,11 @@
+//VARIAVEIS UNITARIAS
+const nome = 'alegria';
+const nome2 = 'tristeza';
+const nome3 = 'nojinho';
+//VARIAVEIS DE ARRAY
+const nomes = ['alegria', 'tristeza', 'nojinho']
+//IMPRIMINDO OS VALORES DO ARRAY
+console.log(nomes)
+console.log(nomes[2])
+nomes[10] = 'medo'//criando dinamicamente
+console.log(nomes[10])
