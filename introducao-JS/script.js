5ce5a16dbb3764e1b156aa786c86b34d10259274
Guto-potato <gustavo.borghi18@gmail.com>
2024-10-17 12:20:43 -0300

    introducao-JS

---
+++ introducao-JS/script.js
@@ -0,0 +1,11 @@
+const idade = 96
+const nome = "jeferson"
+const sobrenome = "rosa da Silva"
+const telefone = "+55 9999 - 9999"
+const estaCasado = true
+
+console.log(idade)
+console.log(nome)
+console.log(sobrenome)
+console.log(telefone)
+console.log(estaCasado)
